46b556c584311318fc0d4d9a875b1dcac8f17bd1
Emilio Botero <ebotero@stanford.edu>
2016-04-15 15:33:44 -0700

    Adding solar UAV optimization

---
+++ Solar_UAV_Optimization/Analyses.py
@@ -0,0 +1,79 @@
+# Analyses.py
+# 
+# Created:  Feb 2015, E. Botero
+# Modified: 
+
+# ----------------------------------------------------------------------        
+#   Imports
+# ----------------------------------------------------------------------    
+
+import SUAVE
+
+# ----------------------------------------------------------------------        
+#   Setup Analyses
+# ----------------------------------------------------------------------  
+
+def setup(configs):
+    
+    analyses = SUAVE.Analyses.Analysis.Container()
+    
+    # build a base analysis for each config
+    for tag,config in configs.items():
+        analysis = base(config)
+        analyses[tag] = analysis
+    
+    return analyses
+
+# ----------------------------------------------------------------------        
+#   Define Base Analysis
+# ----------------------------------------------------------------------  
+
+def base(vehicle):
+    
+    # ------------------------------------------------------------------
+    #   Initialize the Analyses
+    # ------------------------------------------------------------------     
+    analyses = SUAVE.Analyses.Vehicle()
+    
+    # ------------------------------------------------------------------
+    #  Basic Geometry Relations
+    sizing = SUAVE.Analyses.Sizing.Sizing()
+    sizing.features.vehicle = vehicle
+    analyses.append(sizing)
+    
+    # ------------------------------------------------------------------
+    #  Weights
+    # ------------------------------------------------------------------ 
+    weights = SUAVE.Analyses.Weights.Weights()
+    weights.settings.empty_weight_method = SUAVE.Methods.Weights.Correlations.UAV.Empty
+    weights.vehicle = vehicle
+    analyses.append(weights)
+    
+    # ------------------------------------------------------------------
+    #  Aerodynamics Analysis
+    # ------------------------------------------------------------------ 
+    aerodynamics = SUAVE.Analyses.Aerodynamics.Fidelity_Zero()
+    aerodynamics.geometry = vehicle
+    analyses.append(aerodynamics)
+    
+    # ------------------------------------------------------------------
+    #  Energy
+    # ------------------------------------------------------------------ 
+    energy = SUAVE.Analyses.Energy.Energy()
+    energy.network = vehicle.propulsors
+    analyses.append(energy)
+    
+    # ------------------------------------------------------------------
+    #  Planet Analysis
+    # ------------------------------------------------------------------ 
+    planet = SUAVE.Analyses.Planets.Planet()
+    analyses.append(planet)
+    
+    # ------------------------------------------------------------------
+    #  Atmosphere Analysis
+    # ------------------------------------------------------------------ 
+    atmosphere = SUAVE.Analyses.Atmospheric.US_Standard_1976()
+    atmosphere.features.planet = planet.features
+    analyses.append(atmosphere)   
+    
+    return analyses    
